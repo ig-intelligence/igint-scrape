9b3aa227d09e2518027ae300d1d311ccc31e1a8a
Jiayu <yi-jiayu@users.noreply.github.com>
2017-01-22 12:17:27 +0800

    Added limit on maximum number of posts scraped

--- index.js
+++ index.js
@@ -3,15 +3,18 @@
 const debug = require('debug')('igint-scrape:main');
 const express = require('express');
 const logger = require('morgan');
-
 const ig_posts_scraper = require('instagram-screen-scrape');
 
 const app = express();
 app.use(logger('dev'));
 
+const SCRAPE_LIMIT = 50;
+
 function handler(req, res) {
   const username = req.params.username;
   const last_modified = req.get('If-Range') || null;
+
+  console.log(last_modified);
 
   let last_modified_ts;
   if (last_modified) {
@@ -23,11 +26,14 @@ function handler(req, res) {
   posts_stream.on('error', function (err) {
     if (err === 'Instagram returned status code: 404') {
       res.sendStatus(404);
+    } else {
+      console.error(err);
     }
   });
 
   let first_post_out = false;
   let done = false;
+  let count = 0;
 
   const handle_post = function (post) {
     if (!first_post_out) {
@@ -49,6 +55,12 @@ function handler(req, res) {
       }
     }
 
+    count += 1;
+
+    if (count === SCRAPE_LIMIT) {
+      done = true;
+    }
+
     if (done) {
       posts_stream.removeListener('data', handle_post);
       res.write(']');
@@ -62,6 +74,8 @@ function handler(req, res) {
 }
 
 app.get('/:username', handler);
+
+app.get('/version', (req, res) => res.send(require('./package.json').version));
 
 const port = process.env.port || 3000;
 
